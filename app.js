81ce5acea446478f4ace2009429e6ef46011f2f8
zakiraliabro <zakir7865432190@gmail.com>
2024-11-22 00:28:27 +0500

    project-9

---
+++ app.js
@@ -0,0 +1,47 @@
+var greet = "welcome to website!"
+var passwordError = "error! Please enter a valid password"
+let welcome = "Welcome to js land... \n Happy coding!"
+let welcome2 = "Welcome to js land"
+let hcoding = "Happy coding!"
+let chellow = "hellow... i can run js through my web browser's console"
+let username = "Zakir"
+let myname ;
+let a = myanme = "Zakir Ali"
+let message = "Hellow world"
+let studentname = "Jhone Doe" 
+let studentage = "15 year old"
+let certified = "Certified Mobile Applicatio development"
+let pizza ="PIZZA \n PIZZ \n PIZ \n PI \n p"
+let email = "example@example.com"
+let book = "“A smarter way to learn JavaScript"
+let design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+let age = "19 year old"
+let visit = 14
+let VisiterName = "Jhone Doe"
+let ProductTitle = "t-shirt(s)"
+let quantity = 5
+
+
+
+
+
+
+alert (greet)
+alert (passwordError)
+alert (welcome)
+alert (welcome2)
+alert (hcoding)
+console.log (chellow)
+alert (username)
+alert (myanme)
+alert (message)
+alert (studentname)
+alert (studentage)
+alert (certified)
+alert (pizza)
+alert ("My email address is" + " " + email)
+alert ("I am trying to learn from the Book " + book )
+alert (design)
+alert ("i am " + age)
+alert (VisiterName + " " + "ordered" + " " + quantity + " " + ProductTitle + " " + "on XYZ Clothing store.")
+alert ("you have visited this site " + visit + " times")
